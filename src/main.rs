896debfabe26be3593bdf592747e29f3ba221017
Chris Prohoda <cgprohoda@gmail.com>
2017-06-25 22:35:12 -0500

    Basic framework

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate piston_window;
+
+fn main() {
+	let mut window: piston::PistonWindow = piston_window::WindowSettings::new("Space Miner", (960, 540))
+		.exit_on_esc(true)
+		.build()
+		.unwrap_or_else(|error| {panic!("Build failed: ", error)});
+
+	while let Some(event) = window.next() {
+	}
+}
---
+++ src/stage.rs
@@ -0,0 +1,21 @@
+struct Stage {
+	stage: Vec<bool>,
+	rotation: f32,
+}
+
+impl {
+	fn new() {
+		Stage {
+			stage: generate(),
+			rotation: 0f32,
+		}
+	}
+
+	fn rotate(&mut self) {
+		
+	}
+}
+
+fn generate() {
+
+}
