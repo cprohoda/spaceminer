451a721aa031841940e56dc492e438b9fdab7536
Chris Prohoda <cgprohoda@gmail.com>
2018-03-18 23:10:57 -0400

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate piston_window;
+extern crate rand;
 
 fn main() {
-	let mut window: piston::PistonWindow = piston_window::WindowSettings::new("Space Miner", (960, 540))
-		.exit_on_esc(true)
-		.build()
-		.unwrap_or_else(|error| {panic!("Build failed: ", error)});
+    let mut window: piston::PistonWindow = piston_window::WindowSettings::new("Space Miner", (960, 540))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|error| {panic!("Build failed: ", error)});
 
-	while let Some(event) = window.next() {
-	}
+    while let Some(event) = window.next() {
+        match e {
+            Input::
+        }
+    }
 }
