7935264f5b4dae39f1bb0e5ab101319c69c764eb
Chris Prohoda <cgprohoda@gmail.com>
2018-03-20 21:36:12 -0400

    Fix error message

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut window: piston::PistonWindow = piston_window::WindowSettings::new("Space Miner", (960, 540))
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|error| {panic!("Build failed: ", error)});
+        .unwrap_or_else(|error| {panic!("Build failed: {}", error)});
 
     while let Some(event) = window.next() {
         match e {
