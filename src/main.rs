fb20b737cc2060b6d53e37e4f96b2a4e99a44c42
Chris Prohoda <cgprohoda@gmail.com>
2018-03-20 21:49:16 -0400

    Get example problem running

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 extern crate piston_window;
 extern crate rand;
 
+use piston_window::*;
+
 fn main() {
-    let mut window: piston::PistonWindow = piston_window::WindowSettings::new("Space Miner", (960, 540))
+    let mut window: piston_window::PistonWindow = piston_window::WindowSettings::new("Space Miner", [960, 540])
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|error| {panic!("Build failed: {}", error)});
 
     while let Some(event) = window.next() {
-        match e {
-            Input::
-        }
+        window.draw_2d(&event, |context, graphics| {
+            clear([1.0; 4], graphics);
+            rectangle([1.0, 0.0, 0.0, 1.0],
+                      [0.0, 0.0, 100.0, 100.0],
+                      context.transform,
+                      graphics);
+        });
     }
 }
