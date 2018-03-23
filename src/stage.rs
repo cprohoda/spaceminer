72a62d221d2aa1a7a3c7338a5ced07d939675889
Chris Prohoda <cgprohoda@gmail.com>
2018-03-22 23:32:17 -0400

    WIP: updated stage generator algorithm, except that it doesn't modify the stage array. Needs to be diagnosed

--- src/stage.rs
+++ src/stage.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, thread_rng};
 
 struct Stage {
     stage: StageData,
@@ -19,115 +19,127 @@ impl Stage {
         // TODO: add stage rotation
     }
 
-    pub fn update(&mut self, mined: []) {
+    pub fn update(&mut self, mined: Vec<usize>) {
         // TODO: add stage rotation
-        self.stage[]
     }
 }
 
-pub fn generate_stage() -> StageData {
-    let dimension = 500;
-    let mut stage: StageData::Large = [[127; dimension]; dimension];
-    let mut rng = rand::thread_rng();
+pub fn generate_stage() -> [[u8; 10]; 10] {
+    const dimension: usize = 10usize;
+    let mut stage = [[127u8; dimension]; dimension];
+    let mut rng = thread_rng();
     let border_value = 0;
+    let mut center = &mut 127u8;
+    let mut north = 127u8; 
+    let mut northeast = 127u8;
+    let mut east = 127u8;
+    let mut southeast = 127u8;
+    let mut south = 127u8;
+    let mut southwest = 127u8;
+    let mut west = 127u8;
+    let mut northwest = 127u8;
 
     for x in 0..100 {
         for (x, row) in stage.iter().enumerate() {
             for (y, column) in row.iter().enumerate() {
-                let center = &mut stage[x][y];
+                center = &mut stage[x][y];
                 if x > 0 && x < dimension - 1 && y > 0 && y < dimension - 1 {
-                    let north = stage[x-1][y];
-                    let notheast = stage[x-1][y+1];
-                    let east = stage[x][y+1];
-                    let southeast = stage[x+1][y+1];
-                    let south = stage[x+1][y];
-                    let southwest = stage[x+1][y-1];
-                    let west = stage[x][y-1];
-                    let northwest = stage[x-1][y-1];
+                    north = stage[x-1][y];
+                    northeast = stage[x-1][y+1];
+                    east = stage[x][y+1];
+                    southeast = stage[x+1][y+1];
+                    south = stage[x+1][y];
+                    southwest = stage[x+1][y-1];
+                    west = stage[x][y-1];
+                    northwest = stage[x-1][y-1];
                 } else if x > 0 && x < dimension - 1 && y == 0 {
-                    let north = stage[x-1][y];
-                    let notheast = stage[x-1][y+1];
-                    let east = stage[x][y+1];
-                    let southeast = stage[x+1][y+1];
-                    let south = stage[x+1][y];
-                    let southwest = border_value;
-                    let west = border_value;
-                    let northwest = border_value;
+                    north = stage[x-1][y];
+                    northeast = stage[x-1][y+1];
+                    east = stage[x][y+1];
+                    southeast = stage[x+1][y+1];
+                    south = stage[x+1][y];
+                    southwest = border_value;
+                    west = border_value;
+                    northwest = border_value;
                 } else if x > 0 && x < dimension - 1 && y == dimension - 1 {
-                    let north = stage[x-1][y];
-                    let notheast = border_value;
-                    let east = border_value;
-                    let southeast = border_value;
-                    let south = stage[x+1][y];
-                    let southwest = stage[x+1][y-1];
-                    let west = stage[x][y-1];
-                    let northwest = stage[x-1][y-1];
+                    north = stage[x-1][y];
+                    northeast = border_value;
+                    east = border_value;
+                    southeast = border_value;
+                    south = stage[x+1][y];
+                    southwest = stage[x+1][y-1];
+                    west = stage[x][y-1];
+                    northwest = stage[x-1][y-1];
                 } else if x == 0 && y > 0 && y < dimension - 1 {
-                    let north = border_value;
-                    let notheast = border_value;
-                    let east = stage[x][y+1];
-                    let southeast = stage[x+1][y+1];
-                    let south = stage[x+1][y];
-                    let southwest = stage[x+1][y-1];
-                    let west = stage[x][y-1];
-                    let northwest = border_value;
+                    north = border_value;
+                    northeast = border_value;
+                    east = stage[x][y+1];
+                    southeast = stage[x+1][y+1];
+                    south = stage[x+1][y];
+                    southwest = stage[x+1][y-1];
+                    west = stage[x][y-1];
+                    northwest = border_value;
                 } else if x == 0 && y == 0 {
-                    let north = border_value;
-                    let notheast = border_value;
-                    let east = stage[x][y+1];
-                    let southeast = stage[x+1][y+1];
-                    let south = stage[x+1][y];
-                    let southwest = border_value;
-                    let west = border_value;
-                    let northwest = border_value;
+                    north = border_value;
+                    northeast = border_value;
+                    east = stage[x][y+1];
+                    southeast = stage[x+1][y+1];
+                    south = stage[x+1][y];
+                    southwest = border_value;
+                    west = border_value;
+                    northwest = border_value;
                 } else if x == 0 && y == dimension -1 {
-                    let north = border_value;
-                    let notheast = border_value;
-                    let east = border_value;
-                    let southeast = border_value;
-                    let south = stage[x+1][y];
-                    let southwest = stage[x+1][y-1];
-                    let west = stage[x][y-1];
-                    let northwest = stage[x-1][y-1];
+                    north = border_value;
+                    northeast = border_value;
+                    east = border_value;
+                    southeast = border_value;
+                    south = stage[x+1][y];
+                    southwest = stage[x+1][y-1];
+                    west = stage[x][y-1];
+                    northwest = border_value;
                 } else if x == dimension - 1 && y > 0 && y < dimension - 1 {
-                    let north = stage[x-1][y];
-                    let notheast = stage[x-1][y+1];
-                    let east = stage[x][y+1];
-                    let southeast = border_value;
-                    let south = border_value;
-                    let southwest = border_value;
-                    let west = stage[x][y-1];
-                    let northwest = stage[x-1][y-1];
+                    north = stage[x-1][y];
+                    northeast = stage[x-1][y+1];
+                    east = stage[x][y+1];
+                    southeast = border_value;
+                    south = border_value;
+                    southwest = border_value;
+                    west = stage[x][y-1];
+                    northwest = stage[x-1][y-1];
                 } else if x == dimension - 1 && y == 0 {
-                    let north = stage[x-1][y];
-                    let notheast = stage[x-1][y+1];
-                    let east = stage[x][y+1];
-                    let southeast = border_value;
-                    let south = border_value;
-                    let southwest = border_value;
-                    let west = border_value;
-                    let northwest = border_value;
+                    north = stage[x-1][y];
+                    northeast = stage[x-1][y+1];
+                    east = stage[x][y+1];
+                    southeast = border_value;
+                    south = border_value;
+                    southwest = border_value;
+                    west = border_value;
+                    northwest = border_value;
                 } else if x == dimension - 1 && y == dimension - 1 {
-                    let north = stage[x-1][y];
-                    let notheast = border_value;
-                    let east = border_value;
-                    let southeast = border_value;
-                    let south = border_value;
-                    let southwest = border_value;
-                    let west = stage[x][y-1];
-                    let northwest = stage[x-1][y-1];
+                    north = stage[x-1][y];
+                    northeast = border_value;
+                    east = border_value;
+                    southeast = border_value;
+                    south = border_value;
+                    southwest = border_value;
+                    west = stage[x][y-1];
+                    northwest = stage[x-1][y-1];
                 }
 
-                if ( center/2 + rng.gen::<u8>()/2) <= ( center/4 + north/8 + south/8 + east/8 + west/8 + northwest/16 + northeast/16 + southwest/16 + southeast/16 ) { // TODO: find a better averaging algorithm re integer division
-                    if center < 255 {
-                        center += 1;
+                if ( *center/2 + rng.gen::<u8>()/2) <= ( *center/4 + north/8 + south/8 + east/8 + west/8 + northwest/16 + northeast/16 + southwest/16 + southeast/16 ) { // TODO: find a better averaging algorithm re integer division
+                    if center < &mut 255 {
+                        *center = *center + 1;
                     }
                 } else {
-                    if center > 0 {
-                        center -= 1;
+                    if center > &mut 0 {
+                        *center = *center - 1;
                     }
                 }
             }
+        }
+        println!("Stage generator iteration");
+        for x in stage.iter() {
+            println!("{:?}", x);
         }
     }
 
